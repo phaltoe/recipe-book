51b8898d144948e83cf8c1153c3cf700fe57e9e7
phaltoe <reverse9@live.com>
2016-11-23 16:30:12 -0200

    added some recipes

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'rb-recipes',
+  templateUrl: './recipes.component.html'
+})
+export class RecipesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+}
